ee1cf541890105bc5f9d47aca9205a70544c8ec6
YuanZheng <bhu078@uottawa.ca>
2018-02-17 21:07:31 -0500

    payment
    Payment activity

---
+++ app/src/main/java/com/linkedpizza/copay/config/config.java
@@ -0,0 +1,9 @@
+package com.linkedpizza.copay.config;
+
+/**
+ * Created by ${YuanZhengHu} on 2018-02-17.
+ */
+
+public class config {
+    public static final  String PAYPAL_CLIENT_ID = "Af0-IeaXtdYJd6U51hYvlhgIa2rM0Qw96rjAlapbaObqJuPGuRoyzil4h3IwQZsgxJ-E_WITYGl2r3Y_";
+}
