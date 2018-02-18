f3ae38b378953bd2319898b834eb513d0484b74e
Nick <nbroa025@uottawa.ca>
2018-02-17 20:23:38 -0500

    Server json

--- app/src/main/java/com/linkedpizza/copay/MainActivity.java
+++ app/src/main/java/com/linkedpizza/copay/MainActivity.java
@@ -47,10 +47,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         photoURL=getIntent().getExtras().getString("photoURL");
         user = new UserAccount(name,email);
 
-
-
-
-
         makeToast("Welcome" +"\n" + user.toString());
         makeToast(photoURL);
 
