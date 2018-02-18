4bb13a5784f8da93468c24c9cf2f1f48ff66c23b
iDuckDark <ngane103@uottawa.ca>
2018-02-18 10:40:06 -0500

    null

---
+++ app/src/main/java/com/linkedpizza/copay/LocalAccounts.java
@@ -0,0 +1,17 @@
+package com.linkedpizza.copay;
+
+import java.util.ArrayList;
+
+/**
+ * Created by iDarkDuck on 2/18/18.
+ */
+
+public class LocalAccounts {
+
+    private ArrayList<UserAccount> localAccounts;
+
+    private LocalAccounts(){
+        localAccounts = new ArrayList<>(0);
+    }
+
+}
