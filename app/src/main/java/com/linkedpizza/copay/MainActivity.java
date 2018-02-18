66359d82d77c2d9640c1b75760a7713c527ad160
iDuckDark <ngane103@uottawa.ca>
2018-02-17 19:52:14 -0500

    updates

--- app/src/main/java/com/linkedpizza/copay/MainActivity.java
+++ app/src/main/java/com/linkedpizza/copay/MainActivity.java
@@ -1,5 +1,8 @@
 package com.linkedpizza.copay;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -12,23 +15,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Downloader;
+import com.squareup.picasso.Picasso;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     String email;
     String name;
+    String photoURL;
     UserAccount user;
+    Uri uri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
 
+        //creating a user account
         email=getIntent().getExtras().getString("email");
         name=getIntent().getExtras().getString("name");
+        photoURL=getIntent().getExtras().getString("photoURL");
         user = new UserAccount(name,email);
+
+
+
+
+
         makeToast("Welcome" +"\n" + user.toString());
+        makeToast(photoURL);
 
         //from navigation drawer sample code
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -51,6 +75,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //end of navigation view code
+        TextView tv1= (TextView) navigationView.getHeaderView(0).findViewById(R.id.nameTV);
+        tv1.setText(name);
+        TextView tv2= (TextView) navigationView.getHeaderView(0).findViewById(R.id.emailTV);
+        tv2.setText(email);
+
+        if(photoURL!=null){
+            user.setPhotoURL(photoURL);
+            ImageView img = (ImageView) findViewById(R.id.testImage);
+
+            Picasso.with(MainActivity.this).load(photoURL).into(img);
+
+            ImageView userImage = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.profilePicture);
+            Picasso.with(getApplicationContext())
+                    .load(photoURL)
+                    .resize(200, 200)
+                    .centerCrop()
+                    .into(userImage);
+        }
+
     }
     @Override
     public void onBackPressed() {
@@ -111,5 +155,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void makeToast(String s){
         Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
+
+
+        protected Long doInBackground(URL... urls) {
+            int count = urls.length;
+            long totalSize = 0;
+            for (int i = 0; i < count; i++) {
+                if (isCancelled()) break;
+            }
+            return totalSize;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+        }
+
+        protected void onPostExecute(Long result) {
+            //showDialog("Downloaded " + result + " bytes");
+        }
     }
 }
