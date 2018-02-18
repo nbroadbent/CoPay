fe6716319563cc6b3a0e5fc56e8c2d935ce2d9ec
David Goguen <david.goguen@outlook.com>
2018-02-18 00:40:04 -0500

    UI changes

--- app/src/main/java/com/linkedpizza/copay/TopContributorAdapter.java
+++ app/src/main/java/com/linkedpizza/copay/TopContributorAdapter.java
@@ -39,11 +39,12 @@ public class TopContributorAdapter extends ArrayAdapter<UserAccount> {
 
         ((TextView) rowView.findViewById(R.id.txtNumber)).setText("#" + (position + 1));
 
-        //if (user.getPhotoURL() != null) {
-         //   Picasso.with(rowView.getContext()).load(user.getPhotoURL()).into((ImageView) rowView.findViewById(R.id.imgUser));
-        //} else {
-            ((ImageView) rowView.findViewById(R.id.imgUser)).setImageResource(R.drawable.muslim);
-        //}
+        if (user.getPhotoURL() != null) {
+            Picasso.with(rowView.getContext()).load(user.getPhotoURL()).into((ImageView) rowView.findViewById(R.id.imgUser));
+        } else {
+            ((ImageView) rowView.findViewById(R.id.imgUser)).setImageResource(R.drawable.avatar);
+        }
+
         ((TextView) rowView.findViewById(R.id.txtName)).setText(user.getName());
 
         // Return the completed view to render on screen
